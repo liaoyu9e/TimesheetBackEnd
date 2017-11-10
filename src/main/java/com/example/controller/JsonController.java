1e120e620891a51b8b3178b9abb52c35a1a84a6f
Yu Liao <liaoyu9e@gmail.com>
2017-11-10 09:58:28 -0500

    add CrossOrigin annotation

--- src/main/java/com/example/controller/JsonController.java
+++ src/main/java/com/example/controller/JsonController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 public class JsonController {
     @RequestMapping(value = "/addtimesheet", method = RequestMethod.POST)
     public ResponseEntity<ResponseTime> update(@RequestBody UserTime userTime){
