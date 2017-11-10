242b29276938ea523406a09f25bb459e9647639a
Yu Liao <liaoyu9e@gmail.com>
2017-11-10 15:06:41 -0500

    remove ResponseEntity

--- src/main/java/com/example/controller/JsonController.java
+++ src/main/java/com/example/controller/JsonController.java
@@ -2,16 +2,14 @@ package com.example.controller;
 
 import com.example.entity.ResponseTime;
 import com.example.entity.UserTime;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 public class JsonController {
     @RequestMapping(value = "/addtimesheet", method = RequestMethod.POST)
-    public ResponseEntity<ResponseTime> update(@RequestBody UserTime userTime){
+    public ResponseTime update(@RequestBody UserTime userTime){
         ResponseTime rt = new ResponseTime(userTime.getUserID(), "Success");
-        return new ResponseEntity<ResponseTime>(rt, HttpStatus.OK);
+        return rt;
     }
 }
